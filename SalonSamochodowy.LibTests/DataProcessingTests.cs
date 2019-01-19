7f1725da3115e2c444cf68962909a162d255ac5f
Matti <mateusz.szerszen@hotmail.com>
2019-01-19 21:11:18 +0100

    Zmiany w metodach i dodanie testów.

--- SalonSamochodowy.LibTests/DataProcessingTests.cs
+++ SalonSamochodowy.LibTests/DataProcessingTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using SalonSamochodowy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,8 +20,28 @@ namespace SalonSamochodowy.Tests
 
             Samochod iloscWyposarzenia = dp.PobierzSamochodZNajwiekszymWyposazeniem(auta);
             Assert.AreEqual(6, iloscWyposarzenia.ListaWyposazenia.Count);
-
-
         }
+
+        [TestMethod()]
+        public void PobierzSamochodNajdrozszyTest()
+        {
+            List<Samochod> auta = Dane.Dane.Samochody;
+
+            DataProcessing dp = new DataProcessing();
+
+            Samochod wartosc = dp.PobierzSamochodNajdrozszy(auta);
+            Assert.AreEqual(50000, wartosc.CenaBazowa);
+        }
+
+        //[TestMethod()]
+        //public void PosortujPoCenieTest()
+        //{
+        //    List<Samochod> auta = Dane.Dane.Samochody;
+        //    DataProcessing dp = new DataProcessing();
+
+        //    List<Samochod> posortowanaListaAut = dp.PosortujPoCenie(auta);
+
+        //    List<Samochod> doPorownania = new List<Samochod>() { 50000, { 140, Pojemnosc = 1398, Turbo = true }, Model = "A1", RokProdukcji = 2018, SpalanieSrednie = 6.1 }
+        //}
     }
 }
--- SalonSamochodowy/DataProcessing.cs
+++ SalonSamochodowy/DataProcessing.cs
@@ -11,21 +11,19 @@ namespace SalonSamochodowy
         public Samochod PobierzSamochodZNajwiekszymWyposazeniem(List<Samochod> samochody)
         {
             Samochod wynik = samochody.OrderByDescending(s => s.ListaWyposazenia.Count).First();
-           // Samochod wynik =
-               //return (from Samochod s in samochody
-               //          where s.ListaWyposazenia.Max()
-               //          select s);
             return wynik;
         }
 
         public Samochod PobierzSamochodNajdrozszy(List<Samochod> samochody)
         {
-            return null;
+            Samochod wynik = samochody.OrderByDescending(s => s.CenaBazowa).First();
+            return wynik;
         }
 
         public List<Samochod> PosortujPoCenie(List<Samochod> samochody)
         {
-            return null;
+            List<Samochod> wynik = samochody.OrderBy(o=>o.CenaBazowa).ToList(); //OrderByDescending(s => s.CenaBazowa);
+            return wynik;
         }
 
         public bool CzyMaWyposazenie(Samochod s, Wyposazenie w)
